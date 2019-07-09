d0218504484172daebf004ab97bb6e45613293d6
Anthony919498 <villedieu.anthony94@gmail.com>
2019-07-09 22:59:38 +0200

    MAJ2.0

---
+++ js/checkout.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	var existingEntries = JSON.parse(localStorage.getItem("cart_items"));
+	var sous_total = 0;
+	existingEntries.forEach(function(element) {
+
+		$('#purchase_summary').append('<tr><td title="'+ element.text +'">'+ element.text +'</td><td align="center">'+ element.quantity +'</td><td align="right">'+ element.prix * element.quantity + '€' +'</td></tr>');
+		sous_total = (element.prix * element.quantity) + sous_total;
+		console.log(sous_total);
+	});
+
+	$('#sub_total').html(sous_total + '€');
+	$('#total').html(sous_total + +5 + '€');
+});
